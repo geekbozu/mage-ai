20437dc6767de5fe590e6e4d60d3135474aad06f
geekboy1011 <geekboy1011@gmail.com>
2023-04-21 22:02:39 -0400

    Better but not perfect configuration.

--- mage_ai/streaming/sources/nats.py
+++ mage_ai/streaming/sources/nats.py
@@ -5,20 +5,41 @@ from mage_ai.streaming.sources.base import BaseSource
 from mage_ai.streaming.sources.shared import SerializationMethod, SerDeConfig
 from typing import Callable, Dict
 import json
-import nats,asyncio
+import nats
+import nats.errors
+import asyncio
 
 DEFAULT_WAIT_TIME_SECONDS = None
+DEFAULT_CONNECTION_TIMEOUT = 60
+DEFAULT_RECONNECT_ATTEMPTS = -1
+
+
+@dataclass
+class JetstreamConfig:
+    durable_name: str | None = None
+    stream_name: str | None = None
+    batch_size: int = DEFAULT_BATCH_SIZE
 
 
 @dataclass
 class NatsConfig(BaseConfig):
     server_urls: list[str]
     subject_name: str
-    durable_name: str
-    stream_name: str
-    batch_size: int = DEFAULT_BATCH_SIZE
-    # todo security items?
-    wait_time_seconds: int = DEFAULT_WAIT_TIME_SECONDS
+    username: str | None = None
+    password: str | None = None
+    nkey: str | None = None
+    queue_group: str | None = None
+    connection_timeout: int = DEFAULT_CONNECTION_TIMEOUT
+    reconnect_attempts: int = DEFAULT_RECONNECT_ATTEMPTS
+    wait_time_seconds: int | None = DEFAULT_WAIT_TIME_SECONDS
+    jetstream_config: JetstreamConfig | None = None
+
+    @classmethod
+    def parse_config(cls, config: Dict) -> Dict:
+        jetstream_config = config.get('jetstream_config')
+        if jetstream_config and type(jetstream_config) is dict:
+            config['jetstream_config'] = JetstreamConfig(**jetstream_config)
+        return config
 
 
 class NatsSource(BaseSource):
@@ -26,63 +47,88 @@ class NatsSource(BaseSource):
     consume_method = "READ_ASYNC"
 
     def init_client(self):
-        self._print('Start initializing consumer.')
+        self._print("Start initializing consumer.")
         # Initialize nats jetstream consumer
         try:
             loop = asyncio.get_event_loop()
-            self._print(f'Connecting to {self.config.server_urls}')
-            self.nc = loop.run_until_complete(nats.connect(self.config.server_urls,
-                                               connect_timeout=60,
-                                               max_reconnect_attempts=-1)) 
+            self._print(f"Connecting to {self.config.server_urls}")
 
-        # Create JetStream context.
-            self.js = self.nc.jetstream()
-            self.subscription = loop.run_until_complete(self.js.pull_subscribe(self.config.subject_name,
-                                                    self.config.durable_name,
-                                                    self.config.stream_name
-                                                    ))
+            # todo add security context items here.
+            self.nc = loop.run_until_complete(
+                nats.connect(
+                    self.config.server_urls,
+                    connect_timeout=self.config.connection_timeout,
+                    max_reconnect_attempts=self.config.reconnect_attempts,
+                )
+            )
+
+            if self.config.jetstream_config is not None:
+                self._print(
+                    f"Subject: {self.config.subject_name}," +
+                    f" Durable: {self.config.jetstream_config.durable_name}," +
+                    f" Stream: {self.config.jetstream_config.stream_name}")
+
+                self.js = self.nc.jetstream()
+                self.js_subscription = loop.run_until_complete(
+                    self.js.pull_subscribe(
+                        self.config.subject_name,
+                        self.config.jetstream_config.durable_name,
+                        self.config.jetstream_config.stream_name,
+                    )
+                )
+            else:
+                self._print(
+                    f"Subject: {self.config.subject_name}," +
+                    f" Queue: {self.config.queue_group},")
+
+                self.subscription = loop.run_until_complete(
+                    self.nc.subscribe(subject=self.config.subject_name,
+                                      queue=self.config.queue_group)
+                )
+
         except Exception as e:
-            self._print(f'{self.config.server_urls}')
+            self._print(f"{self.config.server_urls}")
             self._print(e)
             raise
         self._print(self.nc.servers)
-        self._print('Finish initializing consumer.')
-
-
-    def read(self, handler: Callable):
-        pass
-    
-    def batch_read(self, handler: Callable):
-        return super().batch_read(handler)
+        self._print("Finish initializing consumer.")
 
     async def read_async(self, handler: Callable):
-        self._print('Start consuming messages.')
-        
-        if self.config.batch_size > 0:
-            batch_size = self.config.batch_size
-        else:
-            batch_size = DEFAULT_BATCH_SIZE
+        self._print("Start consuming messages.")
 
         try:
             while True:
                 try:
-                    messages = await self.subscription.fetch(batch_size,
-                                                        self.config.wait_time_seconds)
+                    messages_to_process = []
+
+                    if self.config.jetstream_config is not None:
+                        messages = await self.js_subscription.fetch(
+                            self.config.jetstream_config.batch_size,
+                            self.config.wait_time_seconds
+                        )
+
+                        for msg in messages:
+                            await msg.ack()
+                            messages_to_process.append(msg.data)
+                    else:
+                        messages_to_process.append(
+                            await self.subscription.next_msg(self.config.wait_time_seconds)
+                        )
                 except nats.errors.TimeoutError:
+
                     continue
 
-                parsed_messages = []
-                for msg in messages:
-                    await msg.ack()
-                    parsed_messages.append(msg.data)
-                    
-                if len(parsed_messages) > 0:
-                    self._print(f'Received {len(parsed_messages)} message. '
-                                f'Sample: {parsed_messages[0]}.')
-                    handler(parsed_messages)
+                if len(messages_to_process) > 0:
+                    handler(messages_to_process)
 
         except Exception:
-            self._print(f'Couldn\'t receive messages from queue {self.config.subject_name}.')
+            self._print(
+                f"Couldn't receive messages from queue {self.config.subject_name}."
+            )
             raise
 
+    def batch_read(self, handler: Callable):
+        return super().batch_read(handler)
 
+    def read(self, handler: Callable):
+        return super().read(handler)
